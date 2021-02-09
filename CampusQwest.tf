77c6243c183117bd0bc36fb6ec6d9d01ee4ff30b
Donald Poland <djpoland@knights.ucf.edu>
2021-02-09 00:35:52 -0500

    Include lambda resources in terraform

--- CampusQwest.tf
+++ CampusQwest.tf
@@ -104,37 +104,63 @@ resource "aws_cognito_identity_pool" "identity_pool" {
     }
 }
 
-# resource "aws_iam_role" "query_dynamodb_lambda" {
-#     name = "query_dynamodb_lambda"
-#     assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Action": "sts:AssumeRole",
-#       "Principal": {
-#         "Service": "lambda.amazonaws.com"
-#       },
-#       "Effect": "Allow",
-#       "Sid": ""
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "query_dynamodb_lambda" {
+    name = "query_dynamodb_lambda"
+    assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
 
-# data "aws_iam_policy_document" "dynamo-access-rw" {
-#     statement {
-#         actions = [
-#             "dyanmodb:DeleteItem",
-#             "dynamodb:GetItem",
-#             "dynamodb:PutItem",
-#             "dynamodb:Query",
-#             "dynamodb:Scan",
-#             "dynamodb:UpdateItem",
-#         ]
-#         resources = [
-#             "arn:aws:dynamodb:::*",
-#         ]
-#     }
-# }
+data "aws_iam_policy_document" "dynamo-access-rw" {
+    statement {
+        actions = [
+            "dyanmodb:DeleteItem",
+            "dynamodb:GetItem",
+            "dynamodb:PutItem",
+            "dynamodb:Query",
+            "dynamodb:Scan",
+            "dynamodb:UpdateItem",
+        ]
+        resources = [
+            "arn:aws:dynamodb:::*",
+        ]
+    }
+}
+
+resource "aws_iam_policy" "dynamo-access-rw" {
+    name = "dynamo-access-rw"
+    path = "/"
+    policy = data.aws_iam_policy_document.dynamo-access-rw.json
+}
+
+resource "aws_iam_role_policy_attachment" "dynamo-access-rw" {
+    role = aws_iam_role.query_dynamodb_lambda.name
+    policy_arn = aws_iam_policy.dynamo-access-rw.arn
+}
+
+resource "aws_iam_role_policy_attachment" "basic-exec-role" {
+    role       = aws_iam_role.query_dynamodb_lambda.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "query_dynamodb_lambda" {
+    filename = "lambda_test.zip"
+    function_name = "query_dynamodb_lambda"
+    role = aws_iam_role.query_dynamodb_lambda.arn
+    handler = "lambda_test.lambda_handler"
+    runtime = "python3.8"
+    timeout = 10
+    source_code_hash = base64sha256("lambda_test.zip")
+}
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Get Virtualenv Directory Path
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -z "$VIRTUAL_ENV_DIR" ]; then
+    VIRTUAL_ENV_DIR="$SCRIPT_DIR/venv"
+fi
+
+echo "Using virtualenv located in : $VIRTUAL_ENV_DIR"
+
+# Create backup of zip if it exists
+if [ -f $SCRIPT_DIR/lambda_test.zip ]; then
+    mv $SCRIPT_DIR/lambda_test.zip $SCRIPT_DIR/lambda_test.zip.backup
+fi
+
+# Add virtualenv libs in new zip file
+cd $VIRTUAL_ENV_DIR/lib/python3.8/site-packages
+zip -r9 $SCRIPT_DIR/lambda_test.zip *
+cd $SCRIPT_DIR
+
+# Add python code in zip file
+zip -r9 $SCRIPT_DIR/lambda_test.zip $SCRIPT_DIR/lambdas/lambda_test.py
+
+# Run terraform apply
+# terraform apply
